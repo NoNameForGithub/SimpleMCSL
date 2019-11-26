31c68a68f10461e21a7d07145a172857ad702195
IntelCoreOcta <46817369+NoNameForGithub@users.noreply.github.com>
2019-11-26 08:53:12 +0100

    Update uninstall.cmd

--- files/uninstall.cmd
+++ files/uninstall.cmd
@@ -6,6 +6,4 @@ if %chch1%==N exit
 :uninstall 
 del main.cmd 
 del uninstall.cmd 
-if exist C:\Users\games\AppData\Roaming/.MinecraftServerLauncher echo Uninstall failed 
-if not exist C:\Users\games\AppData\Roaming/.minecraftserverlauncher echo Uninstalled correctly 
 pause 
