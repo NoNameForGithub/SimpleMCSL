76422f245773a0e4db1ec3714b129ffabe7c72b1
noname <46817369+NoNameForGithub@users.noreply.github.com>
2022-01-06 15:59:16 +0000

    Changes that make you say, why did they do this?
    Preparing to switch over to API v2 so downloads actually work

--- source.cmd
+++ source.cmd
@@ -12,9 +12,9 @@ echo Setting/checking variables..
 set fileshat=http://intelcoreocta.7m.pl/php/mcslfiles.php?dir=%tempdironl1%
 set version=1.0.0
 set dev=1
-set stage=prealpha
-set devn=1.2paper
-set datec=10/07/2020
+set stage=alpha
+set devn=1.2.1paper
+set datec=06/01/2021
 set at=12:12 CEST
 set currentmcver=1.16.1
 set tempddir=%appdata%/.MinecraftServerLauncher
@@ -27,23 +27,11 @@ if %dev%==0 title MCSL-TST v%version% [rewrite]
 if %devmenu%==0 goto menu
 if %devmenu%==1 goto devmenu
 
-::Isolated, untested code
 
-:autoupdate
-echo Checking for updates...
-if exist "%tempddir%\version.txt" del "%tempddir%\version.txt" /q
-set tempdironl1=version-%stage%.txt
-call powershell Invoke-WebRequest %fileshat% -o %tempddir%/version.txt
-if exist "%tempddir%\version.txt" set /p updateversion=<"%tempddir%\version.txt"
-if %updateversion%==%version% goto utd
-set tempdironl1=../source.txt
-call powershell %fileshat% -o %tempddir%/tmpupd/1.bat
-if not exist %tempddir%/updst1.cmd goto stage2-creation
-if exist %tempddir%/updst1.cmd goto stage2ac
-
-::End 
 cls
 goto menu
+
+
 :menu 
 cls
 echo The server toolkit (Compiled on %datec% at %at%)
@@ -58,9 +46,11 @@ IF %s%==l GOTO SMCSN
 IF %s%==i GOTO inst
 if %s%==S goto msgn1
 if %s%==s goto msgn1
+
+
 :inst
 cls
-call powershell Invoke-WebRequest https://papermc.io/api/v1/paper/%currentmcver%/latest/download -o paper.jar
+call powershell Invoke-WebRequest https://papermc.io/api/v2/projects/paper/versions/1.18.1/builds/136/downloads/paper-1.18.1-136.jar -o paper.jar
 java -jar paper.jar %arguments%
 cls
 goto menu
@@ -79,25 +69,9 @@ echo Coming soon!
 pause
 goto menu
 
-::If the stage 2 cmd file doesn't exist, create it
-:stage2-creation
-echo @echo off > %tempddir%/updst1.cmd
-echo set mcdir= >> %tempddir%/updst1.cmd
-echo ren update.bat main.cmd >> %tempddir%/updst1.cmd
-echo mklink MCSL C:\test.lnk
-echo start main.cmd >> %tempddir%/updst1.cmd
-goto autoupdate
-
-:stage2ac
-start %tempddir%/updst1.cmd
-exit
-
 ::Devmenu (feel free to activate)
 echo DEVM3NU/Menu Desarrollador
 execute update 1
 set /p devmo=select=
 if %devmo%==1 goto autoupdate
 
-:uto
-echo Up to date!
-goto menu
